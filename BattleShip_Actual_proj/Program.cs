c581235c01e2a112fadea18918d4170aa2c60704
kyledobash <kyledobash@yahoo.com>
2021-01-11 14:00:45 -0700

    Created classes for various types of ships along with ship parent class, Gameboard class, and Gameplay class.

---
+++ BattleShip_Actual_proj/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip_Actual_proj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ BattleShip_Actual_proj/Submarine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip_Actual_proj
+{
+    class Submarine
+    {
+    }
+}
