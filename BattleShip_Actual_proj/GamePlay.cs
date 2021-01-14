d0d66d2d89e292bc3110c815bdf108ab8014adca
kyledobash <kyledobash@yahoo.com>
2021-01-14 12:37:23 -0700

    Created the UserInterface interface and the GamePlay class skeletons.

---
+++ BattleShip_Actual_proj/GamePlay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip_Actual_proj
+{
+    class GamePlay : UserInterface
+    {
+        //member variables
+
+        //constructor
+
+        //member methods
+
+            // this class will house the master method to execute gameFlow using both UserInterface, and each
+            // classes member methods.
+    }
+}
---
+++ BattleShip_Actual_proj/UserInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip_Actual_proj
+{
+    interface UserInterface
+    {
+        //populate with methods capturing data from the users
+    }
+}
